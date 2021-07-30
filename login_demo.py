acfa4a5c526b7ffe9c775cb1231e47dc0508ca78
bbluray <42467771+bbluray@users.noreply.github.com>
2021-07-30 14:18:23 +0800

    Update and rename demo.py to login_demo.py

--- login_demo.py
+++ login_demo.py
@@ -3,5 +3,6 @@ from BitSrunLogin.LoginManager import LoginManager
 lm = LoginManager()
 lm.login(
     username = "Your srun account",
-    password = "Your password"
+    password = "Your password",
+    # ip = "xx.xx.xx.xx"
 )
