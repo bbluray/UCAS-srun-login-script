72d1393e7dc25cdc2274947b0dc76e9c26f26e47
Peng Liu <littlenewton6@gmail.com>
2024-08-18 21:01:49 +0800

    [*] Change systemd scripts location.

--- utils/linux_systemd/bootstrap.sh
+++ utils/linux_systemd/bootstrap.sh
@@ -6,10 +6,11 @@ sudo git clone https://github.com/LittleNewton/UCAS-srun-login-script.git ${TARG
 sudo chmod +x ${TARGET}/run.py
 
 # 拷贝 systemd 服务和定时器配置文件
-sudo cp ./ucas-login.service /etc/systemd/system/
-sudo cp ./ucas-login.timer /etc/systemd/system/
+sudo cp ${TARGET}/systemd/ucas-login.service /etc/systemd/system/
+sudo cp ${TARGET}/systemd/ucas-login.timer /etc/systemd/system/
 
 # 重载服务
 sudo systemctl daemon-reload
 sudo systemctl enable ucas-login.timer
 sudo systemctl start ucas-login.timer
+sudo journalctl -xeu ucas-login
