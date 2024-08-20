b3befae9277a850d56e554da599ea460073e8959
Peng Liu <littlenewton6@gmail.com>
2024-08-21 00:03:19 +0800

    [+] Add update function.

--- utils/linux_systemd/bootstrap.sh
+++ utils/linux_systemd/bootstrap.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
 # 确保文件存在
 TARGET="/usr/local/bin/auto_login"
-sudo git clone https://github.com/LittleNewton/UCAS-srun-login-script.git ${TARGET}
+# 检查目标文件夹是否存在
+if [ ! -d "$TARGET" ]; then
+    # 文件夹不存在，克隆仓库到指定目录
+    echo "Directory $TARGET does not exist. Cloning repository..."
+    sudo git clone https://github.com/LittleNewton/UCAS-srun-login-script.git "$TARGET"
+else
+    # 文件夹存在，cd进入目录，执行git pull获取最新更新
+    echo "Directory exists. Pulling latest updates..."
+    cd "$TARGET"
+    sudo git pull
+fi
 
 # 确保权限正确
 sudo chmod +x ${TARGET}/run.py
